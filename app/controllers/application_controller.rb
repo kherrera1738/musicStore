ac35edc785b02b8014529122160db6d66633c2cc
Kevyn Herrera <kherrera1738@berkeley.edu>
2021-01-06 19:12:12 -0800

    Finsihed basic website w/ functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include CurrentCart
+  before_action :set_cart
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:name, :email, :password, :password_confirm, :current_password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  resources :instruments
   devise_for :users, controller: {
     registrations: 'registrations'
   }
-  root 'store#index'
+  root 'instruments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+end 
