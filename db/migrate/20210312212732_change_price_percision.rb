8146beb69ffe7deea7f5893bced5bb3d47c60cbd
Kevyn Herrera <kherrera1738@berkeley.edu>
2021-03-12 13:45:44 -0800

    fixed migration issues

--- db/migrate/20210312212732_change_price_percision.rb
+++
@@ -1,6 +0,0 @@
-class ChangePricePercision < ActiveRecord::Migration[6.1]
-  def change
-    change_column :instruments, :price, :decimal, scale: 2
-    change_column :bids, :price, :decimal, scale: 2
-  end
-end
